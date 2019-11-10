6f258367815353584469bdc4dbc8909fc171da9e
Alecsandru Patrascu <alecsandru.patrascu@gmail.com>
2019-11-10 20:36:59 +0000

    updates

--- 02-chroot.sh
+++ 02-chroot.sh
@@ -4,7 +4,7 @@
 
 check_is_root
 
-# Prerequisites
+# Configure mount points, home and locale
 mount none -t proc /proc
 
 mount none -t sysfs /sys
@@ -14,3 +14,85 @@ mount none -t devpts /dev/pts
 export HOME=/root
 
 export LC_ALL=C
+
+# Set a custom hostname
+echo "ubuntu-fs-live" > /etc/hostname
+
+# Configure apt sources.list
+cat <<EOF > /etc/apt/sources.list
+deb http://us.archive.ubuntu.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://us.archive.ubuntu.com/ubuntu/ bionic main restricted universe multiverse
+
+deb http://us.archive.ubuntu.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://us.archive.ubuntu.com/ubuntu/ bionic-security main restricted universe multiverse
+
+deb http://us.archive.ubuntu.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://us.archive.ubuntu.com/ubuntu/ bionic-updates main restricted universe multiverse
+EOF
+
+# Install systemd
+apt-get update
+
+apt-get install -y systemd-sysv
+
+# Configure machine-id and divert
+dbus-uuidgen > /etc/machine-id
+
+ln -fs /etc/machine-id /var/lib/dbus/machine-id
+
+dpkg-divert --local --rename --add /sbin/initctl
+
+ln -s /bin/true /sbin/initctl
+
+# Install packages needed for Live System
+apt-get install -y \
+    ubuntu-standard \
+    casper \
+    lupin-casper \
+    discover \
+    laptop-detect \
+    os-prober \
+    network-manager \
+    resolvconf \
+    net-tools \
+    wireless-tools \
+    wpagui \
+    locales \
+    linux-generic
+
+# Graphical installer
+apt-get install -y \
+    ubiquity \
+    ubiquity-casper \
+    ubiquity-frontend-gtk \
+    ubiquity-slideshow-ubuntu \
+    ubiquity-ubuntu-artwork
+
+# Install window manager
+apt-get install -y \
+    plymouth-theme-ubuntu-logo \
+    ubuntu-gnome-desktop \
+    ubuntu-gnome-wallpapers
+
+# Install useful applications
+apt-get install -y \
+    terminator \
+    apt-transport-https \
+    curl \
+    vim \
+    nano \
+    less
+
+# VS code
+# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+# install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+# echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
+# rm microsoft.gpg
+# apt-get update
+# apt-get install -y code
+
+# Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+apt-get update
+apt-get install google-chrome-stable
